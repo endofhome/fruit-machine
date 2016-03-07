ca3c9ecb1758c5fb5b4d2111325d50f84b69e30a
jamierose <jrose111284@gmail.com>
2016-03-07 17:52:28 +0000

    addFunds money to the wallet

--- app/player.js
+++ app/player.js
@@ -4,7 +4,8 @@ var Player = function() {
   this.wallet = 0;
 };
 
-Player.prototype.addFunds = function() {
+Player.prototype.addFunds = function(money) {
+  this.wallet += money;
 };
 
 module.exports = Player;
--- spec/player.spec.js
+++ spec/player.spec.js
@@ -14,9 +14,14 @@ describe('player', function() {
   });
 
   describe('adding funds to wallet', function() {
-    
+
     it('addFunds method is defined', function() {
       expect(player.addFunds).toBeDefined();
+    });
+
+    it('addFunds money to the wallet', function() {
+      player.addFunds(10);
+      expect(player.wallet).toEqual(10);
     });
   });
 });
